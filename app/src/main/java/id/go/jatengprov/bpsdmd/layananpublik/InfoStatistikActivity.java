004bc330fcac78bf707f66cc6974841861a9e8c5
Donny Malik Kurniawan <bossdony@gmail.com>
2020-09-04 13:50:50 +0700

    up

--- app/src/main/java/id/go/jatengprov/bpsdmd/layananpublik/InfoStatistikActivity.java
+++ app/src/main/java/id/go/jatengprov/bpsdmd/layananpublik/InfoStatistikActivity.java
@@ -3,6 +3,7 @@ package id.go.jatengprov.bpsdmd.layananpublik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class InfoStatistikActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class InfoStatistikActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_statistik);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // todo: goto back activity from here
+
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
